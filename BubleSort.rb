8009e693ed77f31a96ab315f10e2ede447898ac7
Luis Carbuccia <luiscarbuccia@Luiss-MacBook-Pro.local>
2016-12-20 21:34:02 -0500

    no need to reset index in inner loop

--- BubleSort.rb
+++ BubleSort.rb
@@ -6,7 +6,7 @@ def bubble_sort(num_array)
        index = 1
        swapped = false
        while index < num_array.length  do
-           num_array[index], num_array[index-1], swapped, index = num_array[index-1], num_array[index], true, 0 if num_array[index] < num_array[index-1]
+           num_array[index], num_array[index-1], swapped = num_array[index-1], num_array[index], true if num_array[index] < num_array[index-1]
            index += 1
        end
        break unless swapped
